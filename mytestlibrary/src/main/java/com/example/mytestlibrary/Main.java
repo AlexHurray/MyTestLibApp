d943f984ba63db7bdc46ee151e92def3445abe1d
Aurielko <elf.ushi@gmail.com>
2019-02-25 11:37:37 +0300

    Initial commit

---
+++ mytestlibrary/src/main/java/com/example/mytestlibrary/Main.java
@@ -0,0 +1,14 @@
+package com.example.mytestlibrary;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+public class Main {
+
+    public static int test(){
+        Gson gson = new Gson();
+        Log.e("!!!", "oh, yeah!");
+        return 1;
+    }
+}
